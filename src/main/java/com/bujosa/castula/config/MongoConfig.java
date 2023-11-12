284d1ca963641b872b435f83d08ad63115350a93
bujosa <davidbujosa@gmail.com>
2023-11-12 19:03:10 -0400

    feat: add mongo config

---
+++ src/main/java/com/bujosa/castula/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.bujosa.castula.config;
+
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
