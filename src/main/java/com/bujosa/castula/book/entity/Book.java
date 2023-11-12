5e9d60347a4de7e19928f9e0f04e20b2cae08e91
bujosa <davidbujosa@gmail.com>
2023-11-12 18:57:36 -0400

    feat: add mongo collection in book entity

--- src/main/java/com/bujosa/castula/book/entity/Book.java
+++ src/main/java/com/bujosa/castula/book/entity/Book.java
@@ -1,6 +1,12 @@
 package com.bujosa.castula.book.entity;
 
-import lombok.AllArgsConstructor;
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,9 +14,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@Document(collection = "books")
 public class Book {
-    private Long id;
+    @Id
+    private String id;
     private String name;
     private String author;
+    @CreatedDate
+    private Date createdAt;
+
+    @LastModifiedDate
+    private Date updatedAt;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
 }
