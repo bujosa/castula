06c409eefb29274f0843eb67e6a44bf71824950a
bujosa <davidbujosa@gmail.com>
2023-11-12 18:41:46 -0400

    feat: add book dto

---
+++ src/main/java/com/bujosa/castula/book/dtos/BookDto.java
@@ -0,0 +1,23 @@
+package com.bujosa.castula.book.dtos;
+
+import java.time.Instant;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BookDto {
+    private String id;
+    private String name;
+    private String author;
+
+    public BookDto(String name, String author) {
+        // Auto generate the id for the book
+        this.id = Instant.now().toString();
+        this.name = name;
+        this.author = author;
+    }
+}
