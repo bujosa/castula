14fbfccf3fdd4e22ab614108199fd00f6ff18dfb
bujosa <davidbujosa@gmail.com>
2023-11-12 18:30:42 -0400

    feat: init spring boot project

---
+++ src/main/java/com/bujosa/castula/CastulaApplication.java
@@ -0,0 +1,13 @@
+package com.bujosa.castula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bujosa/castula/CastulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bujosa.castula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
